3b96d1f278a2629cf954a8dd8f7e47ed3de8cfe1
Duy Nhat <hdnhat2005@gmail.com>
2025-11-11 20:23:05 +0700

    upload khung project

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+
+// import BUS.AccountServices.LoginAction;
+// import GUI.SaleGroup.LoginGui.LoginFrame.LoginGui;
+import com.formdev.flatlaf.FlatLightLaf;
+
+public class App {
+    public static void main(String[] args) {
+        FlatLightLaf.setup();
+        // new LoginGui();
+    }
+}
---
+++ src/main/java/DataAccess/DatabaseConnector/ConnectManager.java
@@ -0,0 +1,66 @@
+package DataAccess.DatabaseConnector;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.commons.dbutils.DbUtils;
+
+public class ConnectManager {
+    private Connection connect;
+    private String url;
+    private String username;
+    private String password;
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public ConnectManager() {
+        DbUtils.loadDriver(JDBC_DRIVER);
+        Properties properties = new Properties();
+        try {
+            // Đường dẫn tài nguyên trên classpath
+            String resourcePath = "DAL/DatabaseConnector/dbconfig.properties";
+            
+            // Tải file từ classpath
+            InputStream is = getClass().getClassLoader().getResourceAsStream(resourcePath);
+
+            // Rất quan trọng: Kiểm tra xem file có thực sự được tìm thấy không
+            if (is == null) {
+                throw new IOException("Không thể tìm thấy file cấu hình: " + resourcePath);
+            }
+
+            properties.load(is); // Tải từ InputStream
+
+
+            this.url = properties.getProperty("url");
+            this.username = properties.getProperty("username");
+            this.password = properties.getProperty("password");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connect;
+    }
+
+    public void openConnection() {
+        try {
+            this.connect = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            System.out.println("Can't connect database " + url);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            DbUtils.close(connect);
+        } catch (SQLException e) {
+            System.out.println("Something went wrong when closing database");
+            ;
+        }
+    }
+}
