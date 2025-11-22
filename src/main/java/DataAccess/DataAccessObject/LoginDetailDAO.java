150f54fc39d62246b84412795545f0e4fa6a0096
Duy Nhat <hdnhat2005@gmail.com>
2025-11-22 19:21:53 +0700

    fix lại LoginDetailDAO và LoaiSanPham

--- src/main/java/DataAccess/DataAccessObject/LoginDetailDAO.java
+++ src/main/java/DataAccess/DataAccessObject/LoginDetailDAO.java
@@ -1,21 +1,24 @@
 package DataAccess.DataAccessObject;
 
-import DataTransfer.LoaiSanPham;
-
 import java.util.List;
 
-public class LoginDetailDAO  extends  AbstractAccessData<LoaiSanPham.LoginDetail> implements ISimpleAccess<LoaiSanPham.LoginDetail, Integer>{
+import DataTransfer.LoginDetail;
+
+public class LoginDetailDAO  extends  AbstractAccessData<LoginDetail> implements ISimpleAccess<LoginDetail, Integer>{
     {
-        setClazz(LoaiSanPham.LoginDetail.class);
+        setClazz(LoginDetail.class);
     }
     @Override
-    public boolean insert(LoaiSanPham.LoginDetail loginDetail) {
+    public boolean insert(LoginDetail loginDetail) {
         return executeUpdate("INSERT INTO LOGIN_DETAIL(AUTH_KEY, IP_ADDRESS, MAC_ADDRESS,LOGIN_TIME,LOGOUT_TIME,SOTK) VALUES(?,?,?,?,?,?)",
-                loginDetail.getAuthKey(), loginDetail.getIpAddress(), loginDetail.getMacAddress(), loginDetail.getLoginTime(), loginDetail.getLoginTime());
+                loginDetail.getAuthKey(), loginDetail.getIpAddress(), loginDetail.getMacAddress(), loginDetail.getLoginTime(), loginDetail.getLoginTime(), loginDetail.getSoTK());
+
+        //  return executeUpdate("INSERT INTO LOGIN_DETAIL(AUTH_KEY,IP_ADDRESS,MAC_ADDRESS,LOGIN_TIME,LOGOUT_TIME,SOTK) VALUES(?,?,?,?,?,?)",
+        //         loginDetail.getAuthKey(),loginDetail.getIpAddress(),loginDetail.getMacAddress(),loginDetail.getLoginTime(),loginDetail.getLogoutTime(),loginDetail.getSoTK());
     }
 
     @Override
-    public boolean update(Integer loginId, LoaiSanPham.LoginDetail loginDetail) {
+    public boolean update(Integer loginId, LoginDetail loginDetail) {
         return executeUpdate("UPDATE LOGIN_DETAIL SET AUTH_KEY = ?,IP_ADDRESS = ?,MAC_ADDRESS = ?,LOGIN_TIME = ?,LOGOUT_TIME = ?,SOTK = ? WHERE LOGIN_ID = ?",
                 loginDetail.getAuthKey(),loginDetail.getIpAddress(),loginDetail.getMacAddress(),loginDetail.getLoginTime(),loginDetail.getLogoutTime(),loginDetail.getSoTK(),loginId);
     }
@@ -26,16 +29,16 @@ public class LoginDetailDAO  extends  AbstractAccessData<LoaiSanPham.LoginDetail
     }
 
     @Override
-    public LoaiSanPham.LoginDetail select(Integer loginId) {
+    public LoginDetail select(Integer loginId) {
         return executeQuery("SELECT * FROM LOGIN_DETAIL WHERE LOGIN_ID = ?",loginId);
     }
 
     @Override
-    public List<LoaiSanPham.LoginDetail> selectAll() {
+    public List<LoginDetail> selectAll() {
         return executeQueryList("SELECT * FROM LOGIN_DETAIL");
     }
 
-    public LoaiSanPham.LoginDetail selectByAuthKey(String authKey){
+    public LoginDetail selectByAuthKey(String authKey){
         return executeQuery("SELECT * FROM LOGIN_DETAIL WHERE AUTH_KEY = ? AND LOGOUT_TIME >= NOW()",authKey);
     }
 }
