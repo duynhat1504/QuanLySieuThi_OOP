c4b4c1025a69923b91ebe37c58a65d90cf9c9aeb
bitboi-ptit <kpcuongz@gmail.com>
2025-11-22 16:09:07 +0700

    add: NhanVien BUS

---
+++ src/main/java/BUS/BusAccessor/NhanVienBUS.java
@@ -0,0 +1,82 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BUS.BusAccessor;
+
+import DataAccess.DataAccessObject.NhanVienDAO;
+import DataTransfer.HoaDon;
+import DataTransfer.NhanVien;
+import java.util.List;
+
+/**
+ *
+ * @author tiend
+ */
+public class NhanVienBUS implements IBusAccess<NhanVien, Integer> {
+    private final NhanVienDAO nhanVienDAO = new NhanVienDAO();
+
+    public int getSoLuongHoaDon(int manhanvien) {
+        HoaDonBUS hoadonBUS = new HoaDonBUS();
+        int count = 0;
+        List<HoaDon> listHoadon = hoadonBUS.getAll();
+        if (listHoadon == null || listHoadon.isEmpty()) {
+            return 0;
+        }
+        for (HoaDon hoadon : listHoadon) {
+            if (hoadon.getMaNV() == manhanvien) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public long getDoanhThu(int manhanvien) {
+        HoaDonBUS hoadonBUS = new HoaDonBUS();
+        long count = 0;
+        List<HoaDon> listHoadon = hoadonBUS.getAll();
+        if (listHoadon == null || listHoadon.isEmpty()) {
+            return 0;
+
+        }
+        for (HoaDon hoadon : listHoadon) {
+            if (hoadon.getMaNV() == manhanvien) {
+                count += hoadon.getTongTien() - hoadon.getTienGiam();
+            }
+        }
+        return count;
+
+    }
+
+    @Override
+    public NhanVien get(Integer maNhanVien) {
+        return nhanVienDAO.select(maNhanVien);
+    }
+
+    @Override
+    public List<NhanVien> getAll() {
+        return nhanVienDAO.selectAll();
+    }
+
+    @Override
+    public boolean add(NhanVien nhanvien) {
+        return nhanVienDAO.insert(nhanvien);
+    }
+
+    @Override
+    public boolean edit(Integer maNhanVien, NhanVien nhanvien) {
+        return nhanVienDAO.update(maNhanVien, nhanvien);
+    }
+
+    @Override
+    public boolean remove(Integer maNhanVien) {
+        return nhanVienDAO.delete(maNhanVien);
+    }
+
+    @Override
+    public NhanVien getNewest() {
+        List<NhanVien> list = getAll();
+        return list == null ? null : list.get(list.size() - 1);
+    }
+
+}
